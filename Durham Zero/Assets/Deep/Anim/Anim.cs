b8457f4cf59ea5a89e87f0c1d84486eb7dcd9166
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-12-09 18:17:49 +0000

    Setup animations

--- Durham Zero/Assets/Deep/Anim/Anim.cs
+++ Durham Zero/Assets/Deep/Anim/Anim.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
 
 namespace Deep.Anim {
     [CreateAssetMenu(fileName = "Animation", menuName = "Deep/Animation")]
@@ -6,5 +11,48 @@ namespace Deep.Anim {
         public int frameRate = 10;
         public Vector3 offset = Vector3.zero;
         public Sprite[] sprites = new Sprite[0];
+
+        public static Anim Create(Sprite[] frames) {
+            Anim instance = CreateInstance<Anim>();
+            instance.sprites = frames;
+            return instance;
+        }
+    }
+
+    [CustomEditor(typeof(Anim))]
+    public class AnimEditor : Editor {
+        [MenuItem("Assets/Create/Deep/Animation (From Textures)", false, 400)]
+        private static void CreateFromTextures() {
+            Regex trailingNumbersRegex = new Regex(@"(\d+$)");
+
+            List<Sprite> sprites = new List<Sprite>();
+            Texture2D[] textures = Selection.GetFiltered<Texture2D>(SelectionMode.Assets);
+            foreach (Texture2D texture in textures) {
+                string path = AssetDatabase.GetAssetPath(texture);
+                sprites.AddRange(AssetDatabase.LoadAllAssetsAtPath(path).OfType<Sprite>());
+            }
+
+            Sprite[] frames = sprites
+                .OrderBy(
+                    sprite => {
+                        var match = trailingNumbersRegex.Match(sprite.name);
+                        return match.Success ? int.Parse(match.Groups[0].Captures[0].ToString()) : 0;
+                    }
+                )
+                .ToArray();
+
+            Anim asset = Anim.Create(frames);
+            string baseName = trailingNumbersRegex.Replace(textures[0].name, "");
+            asset.name = baseName + "_animation";
+
+            string assetPath = Path.GetDirectoryName(AssetDatabase.GetAssetPath(textures[0]));
+            AssetDatabase.CreateAsset(asset, Path.Combine(assetPath ?? Application.dataPath, asset.name + ".asset"));
+            AssetDatabase.SaveAssets();
+        }
+
+        [MenuItem("Assets/Create/Reanimator/Simple Animation (From Textures)", true, 400)]
+        private static bool CreateFromTexturesValidation() {
+            return Selection.GetFiltered<Texture2D>(SelectionMode.Assets).Length > 0;
+        }
     }
 }
--- Durham Zero/Assets/Player/PlayerController.cs
+++ Durham Zero/Assets/Player/PlayerController.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start() {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
