e339229d97b0bae5fbdbf50719a0e3d03169dc5d
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2023-12-12 05:22:00 +0000

    animations

--- Durham Zero/Assets/Engine/AnimatedEffect.cs
+++ Durham Zero/Assets/Engine/AnimatedEffect.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class AnimatedEffect : MonoBehaviour {
     [SerializeField] private Anim anim;
     public bool loop = false;
+    public bool reverse = false;
     public Color color = Color.white;
 
     private AnimDriver driver = new AnimDriver();
     private SpriteRenderer effect;
 
     private void Start() {
-        driver.Set(anim);
+        driver.Set(anim, reverse ? anim.sprites.Length : -1);
         effect = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (driver.AutoIncrement() && !loop) {
-            Destroy(this);
+        if (reverse) {
+            if (driver.AutoDecrement() && !loop) {
+                Kill();
+            }
+        } else {
+            if (driver.AutoIncrement() && !loop) {
+                Kill();
+            }
         }
         effect.sprite = driver.sprite;
         effect.color = color;
+    }
+
+    private void Kill() {
+        Destroy(this.gameObject);
     }
 }
